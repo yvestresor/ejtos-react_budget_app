18ef8621ba907b9362cfc48af4ad0efdcd62678f
yvestresor <nsyvestresor@gmail.com>
2023-08-31 05:25:57 -0400

    Budget App with React

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 
 import { AppProvider } from './context/AppContext';
@@ -16,17 +20,33 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
                         {/* Add Remaining component here under */}        
-
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
                         {/* Add ExpenseTotal component here under */}        
-                       
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                        <h3 className='mt-3'>Allocation</h3>
                         {/* Add ExpenseList component here under */}         
-
+                        <div className='row '>
+                            <div className='col-sm'>
+                                <ExpenseList />
+                            </div>
+                        </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    
+                    <h3 className='mt-3'>Change allocation</h3>
                         {/* Add AllocationForm component here under */}        
-
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>
+                        </div>
                 </div>
             </div>
         </AppProvider>
